eae5fd861c6bb2df215e6dc726b33d1c211518b5
yuxt <yuxt@live.com>
2021-06-22 23:09:35 +0800

    init

---
+++ App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Forms = System.Windows.Forms;
+
+namespace MinimalSittingNotifier
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Forms.NotifyIcon notifyIcon = new Forms.NotifyIcon();
+            notifyIcon.Icon = new System.Drawing.Icon("Resources/icon.ico");
+            notifyIcon.Visible = true;
+
+            base.OnStartup(e);
+        }
+    }
+}
